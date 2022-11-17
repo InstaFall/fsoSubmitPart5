c5252c15615d309037a78afafa5f0346960ef100
InstaFall <gorkemcanakilli@hotmail.com>
2022-11-17 21:50:05 +0300

    Commit exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,23 @@
-const Blog = ({blog}) => (
-  <div>
-    <b>{blog.title}</b> by {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false)
+  const toggleView = () => {
+    setView(!view)
+  }
+  return view ?
+    <ul className="fullview">
+      <li>Title: {blog.title} <button onClick={toggleView}>hide</button></li>
+      <li>Author: {blog.author}</li>
+      <li>Url: {blog.url}</li>
+      <li>Likes: {blog.likes} <button>like</button></li>
+      <li>id: {blog.id}</li>
+    </ul>
+    :
+    <div>
+      <b>{blog.title}</b> by {blog.author} <button onClick={toggleView}>view</button>
+    </div>
+}
+
 
 export default Blog
