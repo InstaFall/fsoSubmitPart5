d72ed651f43f0e92ae51ebff7e638271c074cf2b
InstaFall <gorkemcanakilli@hotmail.com>
2022-11-18 20:38:36 +0300

    Commit exercise 5.15

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -34,3 +34,16 @@ test('Blog url and likes are displayed after clicking show', async () => {
   screen.getByText('Likes:', { exact:false })
   screen.getByText('Url:', { exact:false })
 })
+
+test('Event handler for likes is called twice if the like button is clicked twice', async () => {
+  const user = userEvent.setup()
+  const likeHandler = jest.fn()
+  const container = render(<Blog blog={testBlog} likeBlog={likeHandler} />).container
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+
+  const likeButton = screen.getByText('like')
+  screen.debug(likeButton)
+  await user.dblClick(likeButton)
+  expect(likeHandler).toHaveBeenCalledTimes(2)
+})
