20413311363f7979d85092ee20413e863eab11aa
InstaFall <gorkemcanakilli@hotmail.com>
2022-11-18 20:15:28 +0300

    Commit exercise 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -11,14 +11,26 @@ const testBlog = {
   user: { username: 'zoooort' }
 }
 
-test('Blog component displays title and author but not url or likes', () => {
+test('Blog component displays title and author but not url or likes',async () => {
   const container = render(<Blog blog={testBlog} />).container
 
-  screen.findByText('Title is rendered')
-  screen.findByText('Author is rendered')
+  await screen.getByText('Title is rendered',{ exact: false })
+  await screen.getByText('Author is rendered', { exact:false })
   const likes = container.querySelector('#blogLikes')
   const url = container.querySelector('#blogUrl')
   screen.debug()
   expect(url).toBeNull()
   expect(likes).toBeNull()
+})
+
+test('Blog url and likes are displayed after clicking show', async () => {
+  const user = userEvent.setup()
+  render(<Blog blog={testBlog} />).container
+  const viewButton = screen.getByText('view')
+  const likes = screen.queryByText('Likes:', { exact:false })
+  expect(likes).toBeNull()
+  await user.click(viewButton)
+  screen.debug()
+  screen.getByText('Likes:', { exact:false })
+  screen.getByText('Url:', { exact:false })
 })
