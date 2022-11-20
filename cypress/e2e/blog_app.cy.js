abe8050762221e8be450dec46a4da1699e0fd484
InstaFall <gorkemcanakilli@hotmail.com>
2022-11-20 03:24:09 +0300

    Commit exercise 5.18

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,11 +1,11 @@
 describe('Blog app', function() {
-  beforeEach(function() {
-    cy.request('POST','http://localhost:3003/api/testing/reset')
+  beforeEach( function() {
     const user = {
       name: 'Phenomenal',
       username: 'phenomenal',
       password: 'uncrackable'
     }
+    cy.request('POST','http://localhost:3003/api/testing/reset')
     cy.request('POST','http://localhost:3003/api/users', user)
     cy.visit('http://localhost:3000')
   })
@@ -18,21 +18,33 @@ describe('Blog app', function() {
     cy.contains('log in').click()
   })
 
-  it('user can login', function() {
-    cy.contains('log in').click()
-    cy.get('#username').type('phenomenal')
-    cy.get('#password').type('uncrackable')
-    cy.get('#login-button').click()
+  describe('Login', async () => {
+    it('succeeds with valid user credentials', () => {
+      cy.contains('log in').click()
+      cy.get('#username').type('phenomenal')
+      cy.get('#password').type('uncrackable')
+      cy.get('#login-button').click()
+      cy.contains('phenomenal logged in')
+    })
 
-    cy.contains('phenomenal logged in')
+    it('fails with wrong credentials ', function() {
+      cy.contains('log in').click()
+      cy.get('#username').type('phenomenal')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain','Invalid credentials')
+        .and('have.css','color','rgb(216, 0, 12)')
+    })
   })
 
-  describe('when logged in', function() {
+  /* describe('when logged in', function() {
     beforeEach(function() {
       cy.contains('log in').click()
       cy.get('#username').type('phenomenal')
       cy.get('#password').type('uncrackable')
       cy.get('#login-button').click()
     })
-  })
+  }) */
 })
